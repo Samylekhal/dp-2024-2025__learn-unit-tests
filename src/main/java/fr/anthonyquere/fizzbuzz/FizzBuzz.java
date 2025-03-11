90e4c1b4d44ab9e9f787ade9e4b0a48164702c00
Samylekhal <samylekhal145@gmail.com>
2025-03-11 11:02:35 +0100

    Fizzbuzz réalisé

--- src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
+++ src/main/java/fr/anthonyquere/fizzbuzz/FizzBuzz.java
@@ -1,14 +1,43 @@
 package fr.anthonyquere.fizzbuzz;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class FizzBuzz {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        List<String> fizzBuzz = startFizzBuzz(15);
+        fizzBuzz.forEach(System.out::println);
     }
 
     public static List<String> startFizzBuzz(int count) {
-        return null;
+        List<String> liste = new ArrayList<String>();
+
+        if (count == 0) {
+            return liste;
+        }
+        else {
+            for (int i = 1 ; i <= count; i++){
+                if(i % 3 == 0 && i  % 5 == 0){
+                    liste.add("FizzBuzz");
+                }
+                else if(i % 3 == 0){
+                    liste.add("Fizz");
+                }
+                else if(i % 5 == 0){
+                    liste.add("Buzz");
+                }
+                else{
+                    liste.add("le reste");
+                }
+
+            }
+            return liste;
+        }
     }
 }
+
+
+
---
+++ src/test/teashopTest.java
@@ -0,0 +1,9 @@
+package fr.anthonyquere.teashop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class teashopTest {
+    
+}
